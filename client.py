51ac482d9734579ceb453ed897ac8e9aaece8840
Alessandro Buratto <bura.alessandro@gmail.com>
2026-01-20 15:36:40 +0100

    Thread safe app

--- client.py
+++ client.py
@@ -4,67 +4,84 @@ from threading import Thread
 import tkinter
 
 
-def receive():
-    """Handles receiving of messages."""
-    while True:
+class ChatClient:
+    """Chat client application with GUI."""
+    
+    def __init__(self, host, port):
+        self.BUFSIZ = 1024
+        self.client_socket = socket(AF_INET, SOCK_STREAM)
+        self.client_socket.connect((host, port))
+        
+        # Create GUI
+        self.top = tkinter.Tk()
+        self.top.title("Chatter")
+        
+        messages_frame = tkinter.Frame(self.top)
+        self.my_msg = tkinter.StringVar()
+        self.my_msg.set("Type your messages here.")
+        scrollbar = tkinter.Scrollbar(messages_frame)
+        
+        self.msg_list = tkinter.Listbox(messages_frame, height=15, width=50, 
+                                         yscrollcommand=scrollbar.set)
+        scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+        self.msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+        self.msg_list.pack()
+        messages_frame.pack()
+        
+        entry_field = tkinter.Entry(self.top, textvariable=self.my_msg)
+        entry_field.bind("<Return>", self.send)
+        entry_field.pack()
+        send_button = tkinter.Button(self.top, text="Send", command=self.send)
+        send_button.pack()
+        
+        self.top.protocol("WM_DELETE_WINDOW", self.on_closing)
+        
+        # Start receive thread
+        receive_thread = Thread(target=self.receive, daemon=True)
+        receive_thread.start()
+    
+    def receive(self):
+        """Handles receiving of messages."""
+        while True:
+            try:
+                msg = self.client_socket.recv(self.BUFSIZ).decode("utf8")
+                if not msg:
+                    break
+                self.msg_list.insert(tkinter.END, msg)
+            except (OSError, ConnectionResetError, ConnectionAbortedError):
+                break
+            except RuntimeError:
+                break
+    
+    def send(self, event=None):
+        """Handles sending of messages."""
+        msg = self.my_msg.get()
+        self.my_msg.set("")
         try:
-            msg = client_socket.recv(BUFSIZ).decode("utf8")
-            msg_list.insert(tkinter.END, msg)
-        except OSError:  # Possibly client has left the chat.
-            break
+            self.client_socket.send(bytes(msg, "utf8"))
+            if msg == "{quit}":
+                self.client_socket.close()
+                self.top.quit()
+        except (OSError, BrokenPipeError):
+            self.top.quit()
+    
+    def on_closing(self, event=None):
+        """This function is to be called when the window is closed."""
+        self.my_msg.set("{quit}")
+        self.send()
+    
+    def run(self):
+        """Start the GUI main loop."""
+        tkinter.mainloop()
 
 
-def send(event=None):  # event is passed by binders.
-    """Handles sending of messages."""
-    msg = my_msg.get()
-    my_msg.set("")  # Clears input field.
-    client_socket.send(bytes(msg, "utf8"))
-    if msg == "{quit}":
-        client_socket.close()
-        top.quit()
-
-
-def on_closing(event=None):
-    """This function is to be called when the window is closed."""
-    my_msg.set("{quit}")
-    send()
-
-top = tkinter.Tk()
-top.title("Chatter")
-
-messages_frame = tkinter.Frame(top)
-my_msg = tkinter.StringVar()  # For the messages to be sent.
-my_msg.set("Type your messages here.")
-scrollbar = tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
-# Following will contain the messages.
-msg_list = tkinter.Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
-scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
-msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
-msg_list.pack()
-messages_frame.pack()
-
-entry_field = tkinter.Entry(top, textvariable=my_msg)
-entry_field.bind("<Return>", send)
-entry_field.pack()
-send_button = tkinter.Button(top, text="Send", command=send)
-send_button.pack()
-
-top.protocol("WM_DELETE_WINDOW", on_closing)
-
-#----Now comes the sockets part----
-HOST = input('Enter host: ')
-PORT = input('Enter port: ')
-if not PORT:
-    PORT = 33000
-else:
-    PORT = int(PORT)
-
-BUFSIZ = 1024
-ADDR = (HOST, PORT)
-
-client_socket = socket(AF_INET, SOCK_STREAM)
-client_socket.connect(ADDR)
-
-receive_thread = Thread(target=receive)
-receive_thread.start()
-tkinter.mainloop()  # Starts GUI execution.
+if __name__ == "__main__":
+    HOST = input('Enter host: ')
+    PORT = input('Enter port: ')
+    if not PORT:
+        PORT = 33000
+    else:
+        PORT = int(PORT)
+    
+    client = ChatClient(HOST, PORT)
+    client.run()
--- server.py
+++ server.py
@@ -4,60 +4,94 @@ from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 
 
-def accept_incoming_connections():
-    """Sets up handling for incoming clients."""
-    while True:
-        client, client_address = SERVER.accept()
-        print("%s:%s has connected." % client_address)
-        client.send(bytes("Greetings from the cave! Now type your name and press enter!", "utf8"))
-        addresses[client] = client_address
-        Thread(target=handle_client, args=(client,)).start()
-
-
-def handle_client(client):  # Takes client socket as argument.
-    """Handles a single client connection."""
-
-    name = client.recv(BUFSIZ).decode("utf8")
-    welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit.' % name
-    client.send(bytes(welcome, "utf8"))
-    msg = "%s has joined the chat!" % name
-    broadcast(bytes(msg, "utf8"))
-    clients[client] = name
-
-    while True:
-        msg = client.recv(BUFSIZ)
-        if msg != bytes("{quit}", "utf8"):
-            broadcast(msg, name+": ")
-        else:
-            client.send(bytes("{quit}", "utf8"))
-            client.close()
-            del clients[client]
-            broadcast(bytes("%s has left the chat." % name, "utf8"))
-            break
-
-
-def broadcast(msg, prefix=""):  # prefix is for name identification.
-    """Broadcasts a message to all the clients."""
-
-    for sock in clients:
-        sock.send(bytes(prefix, "utf8")+msg)
-
+class ChatServer:
+    """Multithreaded chat server."""
+    
+    def __init__(self, host='', port=33000):
+        self.host = host
+        self.port = port
+        self.BUFSIZ = 1024
+        self.clients = {}
+        self.addresses = {}
         
-clients = {}
-addresses = {}
+        self.server = socket(AF_INET, SOCK_STREAM)
+        self.server.bind((self.host, self.port))
+        self.server.listen(5)
+        print(f"Server started on port {self.port}")
+        print("Waiting for connection...")
+        print("Press Ctrl+C to stop the server.")
+    
+    def accept_incoming_connections(self):
+        """Sets up handling for incoming clients."""
+        while True:
+            try:
+                client, client_address = self.server.accept()
+                print("%s:%s has connected." % client_address)
+                client.send(bytes("Greetings from the cave! Now type your name and press enter!", "utf8"))
+                self.addresses[client] = client_address
+                Thread(target=self.handle_client, args=(client,)).start()
+            except OSError:
+                break  # Server socket has been closed
+    
+    def handle_client(self, client):
+        """Handles a single client connection."""
+        
+        name = None
+        try:
+            name = client.recv(self.BUFSIZ).decode("utf8")
+            welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit.' % name
+            client.send(bytes(welcome, "utf8"))
+            msg = "%s has joined the chat!" % name
+            self.broadcast(bytes(msg, "utf8"))
+            self.clients[client] = name
+    
+            while True:
+                msg = client.recv(self.BUFSIZ)
+                if not msg:  # Empty message means client disconnected
+                    break
+                if msg != bytes("{quit}", "utf8"):
+                    self.broadcast(msg, name+": ")
+                else:
+                    client.send(bytes("{quit}", "utf8"))
+                    break
+        except (ConnectionResetError, ConnectionAbortedError, BrokenPipeError, OSError) as e:
+            print(f"Connection error with {name if name else 'unknown client'}: {e}")
+        finally:
+            # Clean up the client connection
+            if client in self.clients:
+                del self.clients[client]
+            if client in self.addresses:
+                del self.addresses[client]
+            try:
+                client.close()
+            except:
+                pass
+            if name:
+                self.broadcast(bytes("%s has left the chat." % name, "utf8"))
+                print(f"{name} has disconnected.")
+    
+    def broadcast(self, msg, prefix=""):
+        """Broadcasts a message to all the clients."""
+        
+        for sock in list(self.clients.keys()):
+            try:
+                sock.send(bytes(prefix, "utf8")+msg)
+            except (OSError, BrokenPipeError):
+                pass  # Client will be removed by handle_client
+    
+    def run(self):
+        """Start the server and handle connections."""
+        accept_thread = Thread(target=self.accept_incoming_connections)
+        accept_thread.start()
+        try:
+            accept_thread.join()
+        except KeyboardInterrupt:
+            print("\nShutting down server...")
+        finally:
+            self.server.close()
+            print("Server closed.")
 
-HOST = ''
-PORT = 33000
-BUFSIZ = 1024
-ADDR = (HOST, PORT)
-
-SERVER = socket(AF_INET, SOCK_STREAM)
-SERVER.bind(ADDR)
 
 if __name__ == "__main__":
-    SERVER.listen(5)
-    print("Waiting for connection...")
-    ACCEPT_THREAD = Thread(target=accept_incoming_connections)
-    ACCEPT_THREAD.start()
-    ACCEPT_THREAD.join()
-    SERVER.close()
+    server = ChatServer(host='', port=33000)
+    server.run()
