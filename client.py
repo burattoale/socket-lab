88462ecdfbdb40d0470cb1f46f5ff6c052a3109d
Alessandro Buratto <bura.alessandro@gmail.com>
2026-01-20 17:34:40 +0100

    Client without gui

--- client.py
+++ client.py
@@ -1,40 +1,16 @@
-"""Script for Tkinter GUI chat client."""
+"""Script for terminal-based chat client."""
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
-import tkinter
 
 
 class ChatClient:
-    """Chat client application with GUI."""
+    """Chat client application for terminal."""
     
     def __init__(self, host, port):
         self.BUFSIZ = 1024
         self.client_socket = socket(AF_INET, SOCK_STREAM)
         self.client_socket.connect((host, port))
-        
-        # Create GUI
-        self.top = tkinter.Tk()
-        self.top.title("Chatter")
-        
-        messages_frame = tkinter.Frame(self.top)
-        self.my_msg = tkinter.StringVar()
-        self.my_msg.set("Type your messages here.")
-        scrollbar = tkinter.Scrollbar(messages_frame)
-        
-        self.msg_list = tkinter.Listbox(messages_frame, height=15, width=50, 
-                                         yscrollcommand=scrollbar.set)
-        scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
-        self.msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
-        self.msg_list.pack()
-        messages_frame.pack()
-        
-        entry_field = tkinter.Entry(self.top, textvariable=self.my_msg)
-        entry_field.bind("<Return>", self.send)
-        entry_field.pack()
-        send_button = tkinter.Button(self.top, text="Send", command=self.send)
-        send_button.pack()
-        
-        self.top.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.running = True
         
         # Start receive thread
         receive_thread = Thread(target=self.receive, daemon=True)
@@ -42,37 +18,51 @@ class ChatClient:
     
     def receive(self):
         """Handles receiving of messages."""
-        while True:
+        while self.running:
             try:
                 msg = self.client_socket.recv(self.BUFSIZ).decode("utf8")
                 if not msg:
+                    print("\n[Server disconnected]")
+                    self.running = False
                     break
-                self.msg_list.insert(tkinter.END, msg)
+                print(f"\n{msg}")
+                print("> ", end="", flush=True)
             except (OSError, ConnectionResetError, ConnectionAbortedError):
-                break
-            except RuntimeError:
+                if self.running:
+                    print("\n[Connection lost]")
+                    self.running = False
                 break
     
-    def send(self, event=None):
+    def send(self, msg):
         """Handles sending of messages."""
-        msg = self.my_msg.get()
-        self.my_msg.set("")
         try:
             self.client_socket.send(bytes(msg, "utf8"))
             if msg == "{quit}":
+                self.running = False
                 self.client_socket.close()
-                self.top.quit()
+                return False
+            return True
         except (OSError, BrokenPipeError):
-            self.top.quit()
-    
-    def on_closing(self, event=None):
-        """This function is to be called when the window is closed."""
-        self.my_msg.set("{quit}")
-        self.send()
+            print("[Connection error]")
+            self.running = False
+            return False
     
     def run(self):
-        """Start the GUI main loop."""
-        tkinter.mainloop()
+        """Start the chat client."""
+        print("Connected to server. Type '{quit}' to exit.")
+        
+        while self.running:
+            try:
+                print("> ", end="", flush=True)
+                msg = input()
+                if not self.send(msg):
+                    break
+            except (KeyboardInterrupt, EOFError):
+                print("\n[Disconnecting...]")
+                self.send("{quit}")
+                break
+        
+        print("Goodbye!")
 
 
 if __name__ == "__main__":
